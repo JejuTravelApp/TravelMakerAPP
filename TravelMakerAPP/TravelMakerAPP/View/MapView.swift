078d9a492a79e5971777322f8d8d6a71dc447ece
Jty98 <clclcfnfn@naver.com>
2024-03-14 17:13:35 +0900

    plist에 위치사용 권한내용 추가

--- TravelMakerAPP/TravelMakerAPP/View/MapView.swift
+++ TravelMakerAPP/TravelMakerAPP/View/MapView.swift
@@ -9,24 +9,13 @@ import SwiftUI
 
 struct MapView: View {
     
-//    @State var ShapeColor: Color = Color.blue
-//    @State var ShapeOpacity: Double = 2.0
-    @State var isAnimating: Bool = false
-    
     var body: some View {
         ZStack {
             Text("ddddd")
-//                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
-//                .frame(width: 260, height: 260, alignment: .center)
-                .offset(y: isAnimating ? 0 : 100) // UIScreen.main.bounds.size.width: 해당 디바이스의 너비값
-                .animation(.easeIn(duration: 1), value: isAnimating)
-                
-        } //: ZSTACk
-        .onAppear(perform: {
-            isAnimating.toggle()
-        })
-    }}
-
+            
+        }
+    }
+}
 //#Preview {
 //    MapView()
 //}
